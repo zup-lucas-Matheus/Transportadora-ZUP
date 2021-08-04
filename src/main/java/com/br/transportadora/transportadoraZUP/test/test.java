efe14c1b3c1388bdd5adc8a12d2bc75ace2205f8
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-04 14:43:42 -0300

    minha branch feature/transportadora

---
+++ src/main/java/com/br/transportadora/transportadoraZUP/test/test.java
@@ -0,0 +1,14 @@
+package com.br.transportadora.transportadoraZUP.test;
+
+import com.br.transportadora.transportadoraZUP.dominio.Fornecedor;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        Fornecedor f = new Fornecedor();
+
+
+
+    }
+}
